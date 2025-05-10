a7f106d21efd397ad7a6e6bfc7d345a9d3a7cbe3
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2025-05-10 13:36:20 +0530

    SignUp Component

--- project/src/app/login/signup/signup.component.ts
+++ project/src/app/login/signup/signup.component.ts
@@ -1,14 +1,28 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, NgClass],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    address: ''
+  };
+
+  onSubmit() {
+    if (this.user.password !== this.user.confirmPassword) return;
+    console.log('Signup successful:', this.user);
+    // Send to server...
+  }
 
 }
